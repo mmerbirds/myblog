0283afca8094b0fa20d93d5c686185692742b6c6
mmerbirds <efaj.edu@gmail.com>
2024-11-12 15:05:51 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# myblog-rs
+
+[Edit on  Mir  ⚡️]()
---
+++ src/Data.js
@@ -0,0 +1,12 @@
+const datas = [
+  {
+    name: 'data',
+    title: 'My Blog 🥳',
+    description:
+      'New and Free template download themes website blog for react.',
+    intro: 'Read Documentation →',
+    img: 'https://mesinkasironline.web.app/img/createwebsiteusingreact.png',
+  },
+];
+
+export default datas;
